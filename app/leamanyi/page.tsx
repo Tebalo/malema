a53ea46a542bb70b6de107a818718d8ca170ecba
bopaki <btebalo@gmail.com>
2023-10-10 01:38:32 +0200

    Created leamayi layout

--- app/leamanyi/page.tsx
+++ app/leamanyi/page.tsx
@@ -1,7 +1,7 @@
 export default function Page(){
     return (
         <main className="flex min-h-screen flex-col items-center justify-between p-24"> 
-            <div className="flex-grow p-4 overflow-y-auto">
+            <div className="flex-grow p-4 overflow-y-auto justify-center w-1/2">
                 <p className="mb-3 text-gray-500 dark:text-gray-400">Track work across the enterprise through an open, collaborative platform. Link issues across Jira and ingest data from other software development tools, so your IT support and operations teams have richer contextual information to rapidly respond to requests, incidents, and changes.</p>
                 <p className="text-gray-500 dark:text-gray-400">Deliver great service experiences fast - without the complexity of traditional ITSM solutions.Accelerate critical development work, eliminate toil, and deploy changes with ease, with a complete audit trail for every change.</p>
                 <p className="mb-3 text-gray-500 dark:text-gray-400">Track work across the enterprise through an open, collaborative platform. Link issues across Jira and ingest data from other software development tools, so your IT support and operations teams have richer contextual information to rapidly respond to requests, incidents, and changes.</p>
