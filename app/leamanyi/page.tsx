80fe2deb372239e6422f32f1efaabc18effcd26e
bopaki <btebalo@gmail.com>
2023-10-10 23:36:44 +0200

    created a stateful list and form submission

--- app/leamanyi/page.tsx
+++ app/leamanyi/page.tsx
@@ -1,19 +1,29 @@
-import React from 'react'
+"use client"
+import React, {useState} from 'react'
 
 interface Item{
     text:string;
 }
 
 export const Page: React.FC = () =>{
-    const items: Item[] = [
-        {
-            text:"Track work across the enterprise through an open, collaborative platform. Link issues across Jira and ingest data from other software development tools, so your IT support and operations teams have richer contextual information to rapidly respond to requests, incidents, and changes."
-        },
-        {
-            text:"Deliver great service experiences fast - without the complexity of traditional ITSM solutions. Accelerate critical development work, eliminate toil, and deploy changes with ease, with a complete audit trail for every change."
+    const [items, setItems] = useState<Item[]>([]); // State for the list of items
+    const [inputText, setInputText] = useState(''); // State for the form input
+
+    const handleSubmit = (e: React.FormEvent) => {
+        e.preventDefault();
+
+        // Create a new item using the input text
+        const newItem: Item = {
+            text: inputText,
         }
-    // Add more items as needed
-    ]
+
+        // Append the new item to the list of items
+        setItems([...items, newItem]);
+
+        // Clear the form input
+        setInputText('');
+    }
+
     return (
         <main className="flex min-h-screen flex-col items-center justify-between p-24"> 
             <div className="flex-grow p-4 overflow-y-auto justify-center w-1/2">
@@ -24,14 +34,23 @@ export const Page: React.FC = () =>{
                     </p>
                 ))}
             </div>
-            <form className="sticky bottom-0 z-10">
+            <form className="sticky bottom-0 z-10" onSubmit={handleSubmit}>
                 <label htmlFor="chat" className="sr-only">Your message</label>
                 <div className="flex items-center px-3 py-2 rounded-lg bg-gray-50 dark:bg-gray-700">
-                    <textarea id="chat" rows={1} className="block mx-4 p-2.5 w-96 text-sm text-gray-900 bg-white rounded-lg border border-gray-300
+                    <textarea 
+                    id="chat" 
+                    rows={1} 
+                    value={inputText}
+                    onChange={(e) => setInputText(e.target.value)}
+                    className="block mx-4 p-2.5 w-96 text-sm text-gray-900 bg-white rounded-lg border border-gray-300
                      focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-800 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white
                       dark:focus:ring-blue-500 dark:focus:border-blue-500 relative z-10" 
-                    placeholder="Send your message..."></textarea>
-                        <button type="submit" className="inline-flex justify-center p-2 text-blue-600 rounded-full cursor-pointer hover:bg-blue-100 dark:text-blue-500 dark:hover:bg-gray-600">
+                    placeholder="Send your message..."
+                    ></textarea>
+                    <button 
+                    type="submit" 
+                    className="inline-flex justify-center p-2 text-blue-600 rounded-full cursor-pointer hover:bg-blue-100
+                     dark:text-blue-500 dark:hover:bg-gray-600">
                         <svg className="w-5 h-5 rotate-90" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 20">
                             <path d="m17.914 18.594-8-18a1 1 0 0 0-1.828 0l-8 18a1 1 0 0 0 1.157 1.376L8 18.281V9a1 1 0 0 1 2 0v9.281l6.758 1.689a1 1 0 0 0 1.156-1.376Z"/>
                         </svg>
