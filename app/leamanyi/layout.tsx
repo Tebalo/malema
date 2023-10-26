acce58f33b2560970ee78e06eac30d551584cc80
bopaki <btebalo@gmail.com>
2023-10-26 23:06:54 +0200

    added responsiveness, response section

--- app/leamanyi/layout.tsx
+++ app/leamanyi/layout.tsx
@@ -24,7 +24,6 @@ export default function RootLayout({
           {/*</aside>*/}
           <NavBar/>
             {children}
-          
         {/*</div>*/}
         </body>
     </html>
--- app/leamanyi/page.tsx
+++ app/leamanyi/page.tsx
@@ -5,6 +5,7 @@ import styles from './custom-scrollbar.module.css';
 
 interface Item{
     text:string;
+    response:string;
 }
 
 export const Page: React.FC = () =>{
@@ -17,6 +18,7 @@ export const Page: React.FC = () =>{
         // Create a new item using the input text
         const newItem: Item = {
             text: inputText,
+            response:inputText,
         }
         // Append the new item to the list of items
         setItems([...items, newItem]);
@@ -29,25 +31,42 @@ export const Page: React.FC = () =>{
             <div className="flex-grow overflow-y-auto justify-center w-full ${styles.customScrollbar}">
                 {/* Map through the list and render each item as a paragraph */}
                 {items.map((item: Item, index: number)=>(
-                    <div className="bg-slate-200 dark:bg-slate-600 mb-1 flex mr-1">
-                        <div className="flex px-96">
-                            <button type="button" className="inline-flex justify-center p-2 text-gray-500 rounded-lg cursor-pointer hover:text-gray-900 hover:bg-gray-100 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600">
-                                <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 18">
-                                    <path fill="currentColor" d="M13 5.5a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0ZM7.565 7.423 4.5 14h11.518l-2.516-3.71L11 13 7.565 7.423Z"/>
-                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M18 1H2a1 1 0 0 0-1 1v14a1 1 0 0 0 1 1h16a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1Z"/>
-                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 5.5a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0ZM7.565 7.423 4.5 14h11.518l-2.516-3.71L11 13 7.565 7.423Z"/>
-                                </svg>
-                                <span className="sr-only">Upload image</span>
-                            </button>
-                            <p key={index} className="mb-3 text-gray-500 dark:text-gray-400 p-2">
-                                {item.text}
-                            </p>
+                    <div className=''>
+                        <div className="bg-slate-200 dark:bg-slate-600 mb-0 flex ">
+                            <div className="flex md:px-10 lg:px-96">
+                                <button type="button" className="inline-flex justify-center p-2 text-gray-500 rounded-lg cursor-pointer hover:text-gray-900 hover:bg-gray-100 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600">
+                                    <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 18">
+                                        <path fill="currentColor" d="M13 5.5a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0ZM7.565 7.423 4.5 14h11.518l-2.516-3.71L11 13 7.565 7.423Z"/>
+                                        <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M18 1H2a1 1 0 0 0-1 1v14a1 1 0 0 0 1 1h16a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1Z"/>
+                                        <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 5.5a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0ZM7.565 7.423 4.5 14h11.518l-2.516-3.71L11 13 7.565 7.423Z"/>
+                                    </svg>
+                                    <span className="sr-only">Upload image</span>
+                                </button>
+                                <p key={index} className="mb-3 text-gray-500 dark:text-gray-400 p-2">
+                                    {item.text}
+                                </p>
+                            </div>
+                        </div>
+                        <div className="bg-slate-200 dark:bg-slate-900 mb-1 flex ">
+                            <div className="flex md:px-10 lg:px-96">
+                                <button type="button" className="inline-flex justify-center p-2 text-gray-500 rounded-lg cursor-pointer hover:text-gray-900 hover:bg-gray-100 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600">
+                                    <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 18">
+                                        <path fill="currentColor" d="M13 5.5a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0ZM7.565 7.423 4.5 14h11.518l-2.516-3.71L11 13 7.565 7.423Z"/>
+                                        <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M18 1H2a1 1 0 0 0-1 1v14a1 1 0 0 0 1 1h16a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1Z"/>
+                                        <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 5.5a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0ZM7.565 7.423 4.5 14h11.518l-2.516-3.71L11 13 7.565 7.423Z"/>
+                                    </svg>
+                                    <span className="sr-only">Upload image</span>
+                                </button>
+                                <p key={index} className="mb-3 text-gray-500 dark:text-gray-400 p-2">
+                                    {item.response}
+                                </p>
+                            </div>
                         </div>
                     </div>
                 ))}
             </div>
             <div className='bg-gray-100 dark:bg-gray-600 sticky bottom-0 w-full justify-center pb-5 pt-2 mx-72'>
-                <form className="z-10 px-96" onSubmit={handleSubmit}>
+                <form className="z-10 md:px-10 lg:px-96 px-1" onSubmit={handleSubmit}>
                     <label htmlFor="chat" className="sr-only">Your message</label>
                     <div className="flex items-center px-3 py-2 rounded-lg bg-gray-50 dark:bg-gray-700">
                         <textarea 
@@ -71,7 +90,7 @@ export const Page: React.FC = () =>{
                         </button>
                     </div>
                 </form>
-                <p className='px-96'>Free Research Preview. PuoGPT may produce inaccurate information about people, places, or facts. PuoGPT September 25 Version</p>
+                <p className='md:px-10 lg:px-96 px-1'>Free Research Preview. PuoGPT may produce inaccurate information about people, places, or facts. PuoGPT September 25 Version</p>
             </div>
         </main>
     );
